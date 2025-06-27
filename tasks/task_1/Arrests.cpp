857adaed357a5fd2758a04926da8b249ad6e6f73
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2025-06-27 00:01:39 -0300

    :sparkles: feat: Task 1

---
+++ tasks/task_1/Arrests.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Arrests.h"
+
+using namespace std;
+
+Arrests::Arrests(int v1, int v2) :  v1_(v1), v2_(v2) {}
---
+++ tasks/task_1/Arrests.h
@@ -0,0 +1,12 @@
+#ifndef ARRESTS_H
+#define ARRESTS_H
+#include <vector> 
+
+class Arrests {
+    public:
+        Arrests(int v1_, int v2);
+        const int v1_;
+        const int v2_;
+};
+
+#endif
